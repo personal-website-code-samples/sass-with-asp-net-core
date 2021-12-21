e689ef83699aa95074b7434dc4aeecc470014479
Michael Bradvica <mjbradvica@protonmail.com>
2021-12-20 20:50:00 -0500

    Add project files.

---
+++ GruntSass/gruntfile.js
@@ -0,0 +1,36 @@
+﻿/// <binding AfterBuild='default' />
+
+module.exports = function (grunt) {
+
+    var sass = require('node-sass');
+
+    // Project configuration.
+    grunt.initConfig({
+
+        // Sass
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: true, // Create source map
+                outputStyle: 'compressed' // Minify output
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true, // Recursive
+                        cwd: "Styles", // The startup directory
+                        src: ["**/*.scss"], // Source files
+                        dest: "wwwroot/css", // Destination
+                        ext: ".css" // File extension
+                    }
+                ]
+            }
+        }
+    });
+
+    // Load the plugin
+    grunt.loadNpmTasks('grunt-sass');
+
+    // Default task(s).
+    grunt.registerTask('default', ['sass']);
+};
